42f521742052ae9b2469012b5e8cd6aafd73c9ea
Fulano de Tal <marcos123201456@gmail.com>
2025-10-17 10:22:16 -0300

    s

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'API Node.js básica rodando 🚀' });
+});
+
+app.get('/usuarios', (req, res) => {
+  res.json([
+    { id: 1, nome: 'Josué' },
+    { id: 2, nome: 'Maria' },
+    { id: 3, nome: 'Carlos' }
+  ]);
+});
+
+app.post('/usuarios', (req, res) => {
+  const usuario = req.body;
+  res.json({ message: 'Usuário recebido com sucesso!', usuario });
+});
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
