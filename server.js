0ff66d5bb25766c4bfe6197b01db6cff76564279
josuearaujomoreira <66573553+josuearaujomoreira@users.noreply.github.com>
2026-05-14 09:16:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.json({ message: 'API Node.js básica rodando 🚀  werwer234' });
+  res.json({ message: 'API Node.js básica rodando 🚀  123' });
 });
 
 app.get('/usuarios', (req, res) => {
